5f71378f9ae2fcc62d7a53dd3624dc6f183ab1f6
Joakim Briceno <joakim.onnhage@gmail.com>
2020-09-01 11:13:19 +0200

    Init commit

---
+++ src/main/java/com/alm2atefsfazlizjoakimo/fortuneteller/FortunetellerApplication.java
@@ -0,0 +1,13 @@
+package com.alm2atefsfazlizjoakimo.fortuneteller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortunetellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FortunetellerApplication.class, args);
+    }
+
+}
