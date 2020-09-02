ff18cc49fc5f6a69bbb1b26788712ce12d837804
Joakim Briceno <joakim.onnhage@gmail.com>
2020-09-02 11:14:19 +0200

    api calls shows in dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+
+const Persons = () => {
+    const [persons, setPersons] = useState([]);
+  const fetchPersons = () => {
+    axios.get("http://localhost:8080/person/all").then(response => {
+      console.log(response);
+      setPersons(response.data);
+    });
+  }
+
+  useEffect(()=> { //Similar to component did mount
+    fetchPersons();
+  }, []) //If there's anything that changes, useEffect will revoke itself
+
+  return persons.map((person, index) => {
+      return (
+          <div key={index}>
+          <h1>{person.name}</h1>
+          <p>{person.id}</p>
+      </div>
+      )
+  })
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Hello</h1>
+        <Persons/>
+      </div>
   );
 }
 
